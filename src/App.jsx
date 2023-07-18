0b258e5a8640cec25a8485eb352b75c3a57386e0
Juan Ignacio Castro <email>
2023-07-17 21:50:44 -0300

    Realizo navbar y footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import React from "react";
+import NavbarComponent from "./components/Navegation/navbar";
+import Footer from "./components/footer";
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import Adminpacientes from "./components/Pages/Admin-pacientes";
+import Adminturnos from "./components/Pages/Admin-turnos";
+import Signin from "./components/Pages/signin";
+import Signup from "./components/Pages/signup";
+import Home from "./components/Pages/Home";
 
+//<NavbarComponent />
 
-function App() {
+const App = () => {
   return (
-    <>
-    </>
-  );
-}
+    
+    <div>
+      <BrowserRouter>
+      <NavbarComponent />
+      <Routes>
+          <Route path="/Home" element={<Home/>}/>
+          <Route path="/Admin-pacientes" element={<Adminpacientes/>}/>
+          <Route path="/Admin-turnos" element={<Adminturnos/>}/>
+          <Route path="/Signin" element={<Signin/>}/>
+          <Route path="/Signup" element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer/>
+      
+    </div>
+      );
+};
+
 
 export default App;
+
+
---
+++ src/components/Pages/Admin-pacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Adminpacientes = () => {
+  return (
+    <div>
+      <h1>Esta es La pagina para administrar pacientes</h1>
+    </div>
+  )
+}
+
+export default Adminpacientes
