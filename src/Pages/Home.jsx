0100d3253311671bcc9e271afb7786b569a22ae6
ConstanzaLop <126610906+ConstanzaLop@users.noreply.github.com>
2023-07-28 20:03:30 -0300

    corrijo section principal responsive

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import '../../styles/styleHome.css';
+import '../styles/styleHome.css';
 
 const Home = () => {
   return (
 <div>
   <main>
       <section className="hero">
-        <div className="row container">
-          <div className="column">
+        <div className="container">
+          <div className="row">
+          <div className="col-lg-6 mt-5">
             <h2>¡Bienvenidos a nuestra veterinaria de confianza!</h2>
             <p> En Pata Sana, nuestro compromiso es proporcionar el más alto nivel de cuidado y cariño a todos los animales que atendemos. Somos un equipo apasionado de médicos veterinarios y profesionales dedicados que comparten una profunda conexión con los animales y una misión común: mejorar la salud y el bienestar de las mascotas.</p>
             <div className="buttons">
@@ -15,8 +16,9 @@ const Home = () => {
               <button className="btn">SUCURSALES</button>
             </div>
           </div>
-          <div className="column">
+          <div className="col-lg-6 my-5">
             <img src="src/img/website-images/images/hero.png" alt="heroImg" class="hero_img"/>
+          </div>
           </div>
         </div>
         <img src="src/img/website-images/images/bg-bottom-hero.png" alt="png" className="curveImg" />
