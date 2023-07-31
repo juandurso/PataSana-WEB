4e5f63df25d2baecdd419a255a00bb021339c164
ConstanzaLop <126610906+ConstanzaLop@users.noreply.github.com>
2023-07-31 16:02:13 -0300

    agrego api clima y js

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,89 @@
 import React from 'react';
 import '../styles/styleHome.css';
 
+
+window.addEventListener('load', () => {
+  let lon;
+  let lat;
+
+  let temperaturaValor = document.getElementById('temperatura-valor');
+  let temperaturaDescripcion = document.getElementById('temperatura-descripcion');
+
+  let ubicacion = document.getElementById('ubicacion');
+  let iconoAnimado = document.getElementById('icono-animado');
+
+  let vientoVelocidad = document.getElementById('viento-velocidad');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(posicion => {
+      lon = posicion.coords.longitude;
+      lat = posicion.coords.latitude;
+
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=Tucuman&appid=6463c6716417643cce6e88a52ee48649`;
+
+      fetch(url)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          let tempKelvin = data.main.temp;
+          let tempCelsius = Math.round(tempKelvin - 273.15);
+          temperaturaValor.textContent = `${tempCelsius} °C`;
+
+          let desc = data.weather[0].description;
+          temperaturaDescripcion.textContent = desc.toUpperCase();
+
+          ubicacion.textContent = data.name;
+
+          let velocidadMs = data.wind.speed;
+          let velocidadKmh = Math.round(velocidadMs * 3.6);
+          vientoVelocidad.textContent = `${velocidadKmh} km/h`;
+
+          console.log(data.weather[0].main);
+          switch (data.weather[0].main) {
+            case 'Thunderstorm':
+              iconoAnimado.src = 'animated/thunder.svg';
+              console.log('TORMENTA');
+              break;
+            case 'Drizzle':
+              iconoAnimado.src = 'animated/rainy-2.svg';
+              console.log('LLOVIZNA');
+              break;
+            case 'Rain':
+              iconoAnimado.src = 'animated/rainy-7.svg';
+              console.log('LLUVIA');
+              break;
+            case 'Snow':
+              iconoAnimado.src = 'animated/snowy-6.svg';
+              console.log('NIEVE');
+              break;
+            case 'Clear':
+              iconoAnimado.src = 'animated/night.svg';
+              iconoAnimado.src = 'animated/day.svg';
+              console.log('LIMPIO');
+              break;
+            case 'Atmosphere':
+              iconoAnimado.src = 'animated/weather.svg';
+              console.log('ATMOSFERA');
+              break;
+            case 'Clouds':
+              iconoAnimado.src = 'animated/cloudy-day-1.svg';
+              console.log('NUBES');
+              break;
+            default:
+              iconoAnimado.src = 'animated/cloudy-day-1.svg';
+              console.log('por defecto');
+          }
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+  }
+});
+
+
+
 const Home = () => {
   return (
 <div>
@@ -24,6 +107,21 @@ const Home = () => {
         <img src="src/img/website-images/images/bg-bottom-hero.png" alt="png" className="curveImg" />
       </section>
   </main>
+
+<div id="contenedor">
+        <div id="caja1">
+            <h1 id="temperatura-valor"></h1>
+            <h2 id="temperatura-descripcion"></h2>
+        </div>
+        <div id="caja2">
+            <h2 id="ubicacion"></h2>
+            <img id="icono-animado" src="" alt="" height="150" width="150"></img>
+        </div>
+        <div id="caja3">
+            <h3>Velocidad del Viento</h3>
+            <h1 id="viento-velocidad"></h1>
+        </div>
+</div>
 
   <section2>
     <div className="wrapper">
