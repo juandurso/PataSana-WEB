79b599b89dfda36c965e5a1625e950c0a62d826e
ConstanzaLop <126610906+ConstanzaLop@users.noreply.github.com>
2023-08-02 19:35:26 -0300

    subo sección cometarios clientes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -82,145 +82,145 @@ window.addEventListener('load', () => {
 });
 
 
-//CARRITO
-let iconCart = document.querySelector('.iconCart');
-let cart = document.querySelector('.cart');
-let container = document.querySelector('.container');
-let close = document.querySelector('.close');
+// //CARRITO
+// let iconCart = document.querySelector('.iconCart');
+// let cart = document.querySelector('.cart');
+// let container = document.querySelector('.container');
+// let close = document.querySelector('.close');
 
-iconCart.addEventListener('click', function(){
-    if(cart.style.right == '-100%'){
-        cart.style.right = '0';
-        container.style.transform = 'translateX(-400px)';
-    }else{
-        cart.style.right = '-100%';
-        container.style.transform = 'translateX(0)';
-    }
-})
-close.addEventListener('click', function (){
-    cart.style.right = '-100%';
-    container.style.transform = 'translateX(0)';
-})
-
-
-let products = null;
-// obtengo datos de json
-fetch('product.json')
-    .then(response => response.json())
-    .then(data => {
-        products = data;
-        addDataToHTML();
-})
-
-//muestra el producto de datos en la lista 
-function addDataToHTML(){
-    // elimina los datos predeterminados de HTML
-    let listProductHTML = document.querySelector('.listProduct');
-    listProductHTML.innerHTML = '';
-
-    // agregar nuevas datos
-    if(products != null) // si tiene datos
-
-    {
-        products.forEach(product => {
-            let newProduct = document.createElement('div');
-            newProduct.classList.add('item');
-            newProduct.innerHTML = 
-            `<img src="src/${product.image}" alt="">
-            <h2>${product.name}</h2>
-            <div class="price">$${product.price}</div>
-            <button onclick="addCart(${product.id})">COMPRAR</button>`;
-
-            listProductHTML.appendChild(newProduct);
-
-        });
-    }
-}
-//usa una cookie para que el carrito no se pierda al actualizar la página
+// iconCart.addEventListener('click', function(){
+//     if(cart.style.right == '-100%'){
+//         cart.style.right = '0';
+//         container.style.transform = 'translateX(-400px)';
+//     }else{
+//         cart.style.right = '-100%';
+//         container.style.transform = 'translateX(0)';
+//     }
+// })
+// close.addEventListener('click', function (){
+//     cart.style.right = '-100%';
+//     container.style.transform = 'translateX(0)';
+// })
 
 
-let listCart = [];
-function checkCart(){
-    var cookieValue = document.cookie
-    .split('; ')
-    .find(row => row.startsWith('listCart='));
-    if(cookieValue){
-        listCart = JSON.parse(cookieValue.split('=')[1]);
-    }else{
-        listCart = [];
-    }
-}
-checkCart();
-function addCart($idProduct){
-    let productsCopy = JSON.parse(JSON.stringify(products));
-    // si este producto no esta en el carrito
-    if(!listCart[$idProduct]) 
-    {
-        listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
-        listCart[$idProduct].quantity = 1;
-    }else{
-        //si este producto ya esta en el carrito
-        //aumeto cantidad
-        listCart[$idProduct].quantity++;
-    }
-    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+// let products = null;
+// // obtengo datos de json
+// fetch('product.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         products = data;
+//         addDataToHTML();
+// })
 
-    addCartToHTML();
-}
-addCartToHTML();
-function addCartToHTML(){
-    // borrar datos por defecto
-    let listCartHTML = document.querySelector('.listCart');
-    listCartHTML.innerHTML = '';
+// muestra el producto de datos en la lista 
+// function addDataToHTML(){
+//     elimina los datos predeterminados de HTML
+//     let listProductHTML = document.querySelector('.listProduct');
+//     listProductHTML.innerHTML = '';
 
-    let totalHTML = document.querySelector('.totalQuantity');
-    let totalQuantity = 0;
-    // si tiene producto en el carrito
-    if(listCart){
-        listCart.forEach(product => {
-            if(product){
-                let newCart = document.createElement('div');
-                newCart.classList.add('item');
-                newCart.innerHTML = 
-                    `<img src="${product.image}">
-                    <div class="content">
-                        <div class="name">${product.name}</div>
-                        <div class="price">$${product.price} / 1 product</div>
-                    </div>
-                    <div class="quantity">
-                        <button onclick="changeQuantity(${product.id}, '-')">-</button>
-                        <span class="value">${product.quantity}</span>
-                        <button onclick="changeQuantity(${product.id}, '+')">+</button>
-                    </div>`;
-                listCartHTML.appendChild(newCart);
-                totalQuantity = totalQuantity + product.quantity;
-            }
-        })
-    }
-    totalHTML.innerText = totalQuantity;
-}
-function changeQuantity($idProduct, $type){
-    switch ($type) {
-        case '+':
-            listCart[$idProduct].quantity++;
-            break;
-        case '-':
-            listCart[$idProduct].quantity--;
+//     // agregar nuevas datos
+//     // if(products != null) // si tiene datos
 
-            // si la cantidad <= 0 entonces elimina el producto del carrito
-            if(listCart[$idProduct].quantity <= 0){
-                delete listCart[$idProduct];
-            }
-            break;
+//     {
+//         products.forEach(product => {
+//             let newProduct = document.createElement('div');
+//             newProduct.classList.add('item');
+//             newProduct.innerHTML = 
+//             `<img src="src/${product.image}" alt="">
+//             <h2>${product.name}</h2>
+//             <div class="price">$${product.price}</div>
+//             <button onclick="addCart(${product.id})">COMPRAR</button>`;
+
+//             listProductHTML.appendChild(newProduct);
+
+//         });
+//     }
+// }
+// usa una cookie para que el carrito no se pierda al actualizar la página
+
+
+// let listCart = [];
+// function checkCart(){
+//     var cookieValue = document.cookie
+//     .split('; ')
+//     .find(row => row.startsWith('listCart='));
+//     if(cookieValue){
+//         listCart = JSON.parse(cookieValue.split('=')[1]);
+//     }else{
+//         listCart = [];
+//     }
+// }
+// checkCart();
+// function addCart($idProduct){
+//     let productsCopy = JSON.parse(JSON.stringify(products));
+//     si este producto no esta en el carrito
+//     if(!listCart[$idProduct]) 
+//     {
+//         listCart[$idProduct] = productsCopy.filter(product => product.id == $idProduct)[0];
+//         listCart[$idProduct].quantity = 1;
+//     }else{
+//         si este producto ya esta en el carrito
+//         //aumeto cantidad
+//         listCart[$idProduct].quantity++;
+//     }
+//     document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+
+//     addCartToHTML();
+// }
+// addCartToHTML();
+// function addCartToHTML(){
+//     borrar datos por defecto
+//     let listCartHTML = document.querySelector('.listCart');
+//     listCartHTML.innerHTML = '';
+
+//     let totalHTML = document.querySelector('.totalQuantity');
+//     let totalQuantity = 0;
+//     si tiene producto en el carrito
+//     if(listCart){
+//         listCart.forEach(product => {
+//             if(product){
+//                 let newCart = document.createElement('div');
+//                 newCart.classList.add('item');
+//                 newCart.innerHTML = 
+//                     `<img src="${product.image}">
+//                     <div class="content">
+//                         <div class="name">${product.name}</div>
+//                         <div class="price">$${product.price} / 1 product</div>
+//                     </div>
+//                     <div class="quantity">
+//                         <button onclick="changeQuantity(${product.id}, '-')">-</button>
+//                         <span class="value">${product.quantity}</span>
+//                         <button onclick="changeQuantity(${product.id}, '+')">+</button>
+//                     </div>`;
+//                 listCartHTML.appendChild(newCart);
+//                 totalQuantity = totalQuantity + product.quantity;
+//             }
+//         })
+//     }
+//     totalHTML.innerText = totalQuantity;
+// }
+// function changeQuantity($idProduct, $type){
+//     switch ($type) {
+//         case '+':
+//             listCart[$idProduct].quantity++;
+//             break;
+//         case '-':
+//             listCart[$idProduct].quantity--;
+
+//             si la cantidad <= 0 entonces elimina el producto del carrito
+//             if(listCart[$idProduct].quantity <= 0){
+//                 delete listCart[$idProduct];
+//             }
+//             break;
     
-        default:
-            break;
-    }
-    // guardar nuevos datos en la cookie
-    document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
-    //  recargar el carrito de vista html
-    addCartToHTML();
-}
+//         default:
+//             break;
+//     }
+//     guardar nuevos datos en la cookie
+//     document.cookie = "listCart=" + JSON.stringify(listCart) + "; expires=Thu, 31 Dec 2025 23:59:59 UTC; path=/;";
+//      recargar el carrito de vista html
+//     addCartToHTML();
+// }
 
 //FIN CARRITO
 
@@ -543,6 +543,71 @@ const Home = () => {
     </div>
   </div>
   </section7>
+
+  <section8>
+  <div className="wrapper">
+    <div className="box">
+      <i className="fas fa-quote-left quote"></i>
+      <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
+      <div className="content">
+        <div className="info">
+          <div className="name">Julieta Castro</div>
+          <div className="job">Facebook | Juli Castro</div>
+          <div className="stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+          </div>
+        </div>
+        <div class="image">
+          <img src="images/profile-1.jpeg" alt=""></img>
+        </div>
+      </div>
+    </div>
+    <div className="box">
+      <i className="fas fa-quote-left quote"></i>
+      <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
+      <div className="content">
+        <div className="info">
+          <div className="name">Marcelo Lobos</div>
+          <div className="job">Facebook | Marce Wolf</div>
+          <div className="stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="far fa-star"></i>
+            <i className="far fa-star"></i>
+          </div>
+        </div>
+        <div className="image">
+          <img src="images/profile-2.jpeg" alt=""></img>
+        </div>
+      </div>
+    </div>
+    <div className="box">
+      <i className="fas fa-quote-left  quote"></i>
+      <p>Lorem aliasry ipsum dolor sits ametans, consectetur adipisicing elitits. Expedita reiciendis itaque placeat thuratu, quasi yiuos repellendus repudiandae deleniti ideas fuga molestiae, alias.</p>
+      <div className="content">
+        <div className="info">
+          <div className="name">Karina Medina</div>
+          <div className="job">Facebook | Karina Medina</div>
+          <div className="stars">
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="fas fa-star"></i>
+            <i className="far fa-star"></i>
+          </div>
+        </div>
+        <div className="image">
+          <img src="images/profile-3.jpeg" alt=""></img>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section8>
 
 
 
