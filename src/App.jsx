66c030e26bae9f99b395aa99f14e96f46bac39b9
FabianBriz <angelfabianbrizuela@gmail.com>
2023-08-09 22:00:31 -0300

    agrego el form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,119 @@
+import React, {useState} from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import "./Contacto.css"
+
+const Formulario = () => {
+	const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
+	return (
+		<>
+			<Formik
+				initialValues={{
+					nombre: '',
+					correo: ''
+				}}
+				validate={(valores) => {
+					let errores = {};
+
+					// Validacion nombre
+					if(!valores.nombre){
+						errores.nombre = 'Por favor ingresa un nombre'
+					} else if(!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(valores.nombre)){
+						errores.nombre = 'El nombre solo puede contener letras y espacios'
+					}
+
+					// Validacion correo
+					if(!valores.correo){
+						errores.correo = 'Por favor ingresa un correo electronico'
+					} else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.correo)){
+						errores.correo = 'El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.'
+					}
+
+					return errores;
+				}}
+				onSubmit={(valores, {resetForm}) => {
+					resetForm();
+					console.log('Formulario enviado');
+					cambiarFormularioEnviado(true);
+					setTimeout(() => cambiarFormularioEnviado(false), 5000);
+				}}
+			>
+				{( {errors} ) => (
+					<Form className="formulario">
+						<div>
+							<label htmlFor="nombre">Nombre</label>
+							<Field
+								type="text" 
+								id="nombre" 
+								name="nombre" 
+								placeholder="John Doe"
+							/>
+							<ErrorMessage name="nombre" component={() => (<div className="error">{errors.nombre}</div>)} />
+						</div>
+						<div>
+							<label htmlFor="correo">Correo</label>
+							<Field
+								type="text" 
+								id="correo" 
+								name="correo" 
+								placeholder="correo@correo.com" 
+							/>
+							<ErrorMessage name="correo" component={() => (<div className="error">{errors.correo}</div>)} />
+						</div>
+						<div>
+							<label>
+								<Field type="radio" name="sexo" value="hombre" /> Hombre
+							</label>
+							<label>
+								<Field type="radio" name="sexo" value="mujer" /> Mujer
+							</label>
+						</div>
+
+						<div>
+							<Field name="mensaje" as="textarea" placeholder="Mensaje" />
+						</div>
+
+						<button type="submit">Enviar</button>
+						{formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+					</Form>
+				)}
 
 
-function App() {
-  return (
-    <>
-    </>
-  );
+				{/* {( {values, errors, touched, handleSubmit, handleChange, handleBlur} ) => (
+					<form className="formulario" onSubmit={handleSubmit}>
+						<div>
+							<label htmlFor="nombre">Nombre</label>
+							<input 
+								type="text" 
+								id="nombre" 
+								name="nombre" 
+								placeholder="John Doe" 
+								value={values.nombre}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+
+							{touched.nombre && errors.nombre && <div className="error">{errors.nombre}</div>}
+						</div>
+						<div>
+							<label htmlFor="correo">Correo</label>
+							<input 
+								type="text" 
+								id="correo" 
+								name="correo" 
+								placeholder="correo@correo.com" 
+								value={values.correo}
+								onChange={handleChange}
+								onBlur={handleBlur}
+							/>
+							{touched.correo && errors.correo && <div className="error">{errors.correo}</div>}
+						</div>
+						<button type="submit">Enviar</button>
+						{formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+					</form>
+				)} */}
+			</Formik>
+		</>
+	);
 }
-
-export default App;
+ 
+export default Formulario;
