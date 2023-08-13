c93385a00d203d3154b0c694a2e2e2ef1d280253
juandurso <juandurso1@gmail.com>
2023-08-13 01:06:34 -0300

    Correcion Versel

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,9 @@
 import React from "react";
 import NavbarComponent from "./components/Navegation/navbar";
 import Footer from "./components/footer";
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Adminpacientes from "./Pages/Admin-pacientes";
 import Adminturnos from "./Pages/Admin-turnos";
-import SignIn from "./Pages/SignIn";
 // import signUp from "./Pages/signup";
 import SignUp2 from "./Pages/SignUp2";
 import Home from "./Pages/Home";
@@ -13,29 +12,15 @@ import AcercaDeNosotros from "./Pages/Acerca-de-nosotros";
 import Error404 from "./Pages/Error404";
 // FIN MIO
 
-import Footer from "../src/components/footer";
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Adminpacientes from "./Pages/Admin-pacientes";
-import Adminturnos from "./Pages/Admin-turnos";
-import Signin from "./Pages/signin";
-import Signup from "./Pages/signup";
+import SignIn from "./Pages/signin";
 import Home from "./Pages/Home";
 
-import Error404 from "./Pages/Error404";
-
-
-import DetallesDePlanes from "./Pages/DetallesDePlanes"
-
+import DetallesDePlanes from "./Pages/DetallesDePlanes";
 
 //<NavbarComponent />
 
 const App = () => {
-
-
-
-
   return (
-
     <div>
       <BrowserRouter>
         <NavbarComponent />
@@ -43,22 +28,19 @@ const App = () => {
           <Route path="/Home" element={<Home />} />
           <Route path="/Admin-pacientes" element={<Adminpacientes />} />
           <Route path="/Admin-turnos" element={<Adminturnos />} />
-          <Route path="/Signin" element={<SignIn/>} />
+          <Route path="/Signin" element={<SignIn />} />
           {/* <Route path="/Signup" element={<signup />} /> */}
-          <Route path="/Signup2" element= {<SignUp2 />} />
+          <Route path="/Signup2" element={<SignUp2 />} />
           <Route path="/aboutus" element={<AcercaDeNosotros />} />
 
-          <Route path="/error404" element={<Error404/>}/>
+          <Route path="/error404" element={<Error404 />} />
 
-          <Route path="/DetallesDePlanes"element={<DetallesDePlanes/>}/>
+          <Route path="/DetallesDePlanes" element={<DetallesDePlanes />} />
         </Routes>
       </BrowserRouter>
       <Footer />
-
     </div>
   );
-	
-
 };
 
 export default App;
