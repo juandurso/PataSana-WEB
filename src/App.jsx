c09b3ec1f3a121a5451e9e028aeb40ca23eca273
juandurso <juandurso1@gmail.com>
2023-08-12 21:16:00 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ import Footer from "../src/components/footer";
 import {Routes, Route, BrowserRouter} from 'react-router-dom';
 import Adminpacientes from "./Pages/Admin-pacientes";
 import Adminturnos from "./Pages/Admin-turnos";
-import Signin from "./Pages/signin";
 import Signup from "./Pages/signup";
 import Home from "./Pages/Home";
 
