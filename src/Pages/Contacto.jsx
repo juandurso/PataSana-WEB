aa29445cd7043e5ea8a75cbcbe677b0db4623563
ConstanzaLop <126610906+ConstanzaLop@users.noreply.github.com>
2023-08-13 21:51:23 -0300

    inicio rama contacto

---
+++ src/Pages/Contacto.jsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from 'react';
+import emailjs from 'emailjs-com';
+import toast, { Toaster } from 'react-hot-toast';
+
+
+
+
+const Contacto = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Contacto
