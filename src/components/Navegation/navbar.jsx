bf2dca31287fa17faed9ac7908abc42752bcae2f
juandurso <juandurso1@gmail.com>
2023-08-13 23:25:33 -0300

    modifico nav

--- src/components/Navegation/navbar.jsx
+++ src/components/Navegation/navbar.jsx
@@ -5,7 +5,6 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import '../../styles/styleNavBar.css';
-// import logoNavbar from '../../../src/img/logonavbar.png';
 import Image from 'react-bootstrap/Image';
 
 
