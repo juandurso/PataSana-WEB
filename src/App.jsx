c1750a09de3d16258f17eed68d24ea7ed9a91832
juandurso <juandurso1@gmail.com>
2023-08-14 14:23:08 -0300

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
           <Route path="/Home" element={<Home />} />
           <Route path="/AdminPacientes" element={<Adminpacientes />} />
           <Route path="/Admin-turnos" element={<Adminturnos />} />
-          <Route path="/Signup" element={<SignUp />} />
+          {/* <Route path="/Signup" element={<SignUp />} /> */}
           <Route path="/aboutus" element={<AcercaDeNosotros />} />
           <Route path="/error404" element={<Error404 />} />
           <Route path="/DetallesDePlanes" element={<DetallesDePlanes />} />
